aaf67fc97801d830164c11f7091c7fa459cfddbc
0784111172 <kaweranelly@gmail.com>
2023-01-27 07:04:52 -0800

    message

---
+++ index.js
@@ -0,0 +1,8 @@
+const save_button=document.querySelector(".save")
+const read_button = document.querySelector(".read")
+
+
+const username="nelly"
+const passward="123"
+localStorage.setItem("User",username)
+localStorage.setItem("passward",passward)
