745b81972d901fb27cb8f6c4b927155a5f6009b4
0784111172 <kaweranelly@gmail.com>
2023-02-15 21:03:41 -0800

    Added blog dashboard

---
+++ src/script/dashbord.js
@@ -0,0 +1,26 @@
+// Get the buttons
+const createBlogButton = document.getElementById('create-blog-button');
+const updateBlogButton = document.getElementById('update-blog-button');
+const readBlogButton = document.getElementById('read-blog-button');
+const deleteBlogButton = document.getElementById('delete-blog-button');
+
+// Add click event listeners to the buttons
+createBlogButton.addEventListener('click', () => {
+    // Code to create a blog goes here
+    console.log('Create blog button clicked');
+});
+
+updateBlogButton.addEventListener('click', () => {
+    // Code to update a blog goes here
+    console.log('Update blog button clicked');
+});
+
+readBlogButton.addEventListener('click', () => {
+    // Code to read a blog goes here
+    console.log('Read blog button clicked');
+});
+
+deleteBlogButton.addEventListener('click', () => {
+    // Code to delete a blog goes here
+    console.log('Delete blog button clicked');
+});
